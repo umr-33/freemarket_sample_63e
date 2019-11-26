dbf9a54451d74885c34c350f8e7840a5eaad64e2
nana0707 <hydrangeaheartlessness@gmail.com>
2019-11-26 19:19:13 +0900

    新規登録ページの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,20 @@
 class UsersController < ApplicationController
   def show
   end
+
+  def new1
+    render "users/new/signin1"
+  end
+  def new2
+    render "users/new/signin2"
+  end
+  def new3
+    render "users/new/signin3"
+  end
+  def new4
+    render "users/new/signin4"
+  end
+  def new5
+    render "users/new/signin5"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   root to: "items#index"
   resources :items
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show] do
+    collection do
+      get "new1", to: "users#new1"
+      get "new2", to: "users#new2"
+      get "new3", to: "users#new3"
+      get "new4", to: "users#new4"
+      get "new5", to: "users#new5"
+    end
+  end
 end
