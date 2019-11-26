b8fc398b8844f873dcfcb004b838999a7f1e1aca
takanari-saiki <stakanari987@gmail.com>
2019-11-26 14:42:55 +0900

    csv file and seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "csv"
+
+count = 1
+CSV.foreach('db/categories.csv') do |row|
+  if count == 1
+    Category.create!(id: count, name: row[0])
+    count += 1
+  else
+    #Category.find(count).destroy if count < 358
+    Category.create!(id: count, name: row[0], parent_id: row[1])
+    count += 1
+  end
+end
