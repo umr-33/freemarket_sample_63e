a66e5b7a77b614a9bb8e5e56fbf79056290a4096
Green-Grain <zwei.blattes@gmail.com>
2019-11-27 19:31:30 +0900

    markup+css

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,7 +10,6 @@ class ItemsController < ApplicationController
     @item = Item.create(item_params)
     image_params[:image].each do |img|
       @item.images.create(image: img)
-      # Image.create(image: img, user_id: @item.id)
     end
   end
 
@@ -20,7 +19,6 @@ class ItemsController < ApplicationController
     params.require(:item).permit(
       :title, :description, :prefecture_id, :shipping_charger_id, :shipping_method_id,
       :days_to_ship_id, :item_status_id, :trade_method_id, :brand_id, :category_id).merge(user_id: current_user.id)
-    
   end
 
   def image_params
