ed532dce8c054e355e3964fc3a31f5f49b061d79
takanari-saiki <stakanari987@gmail.com>
2019-11-28 17:46:16 +0900

    made brand routing and search function

---
+++ app/controllers/brands_controller.rb
@@ -0,0 +1,12 @@
+class BrandsController < ApplicationController
+  def index
+    @brand_cats = Brand.find(1).children
+  end
+  def show
+    @brand_cats = Brand.find(1).children
+    @brand_cat = Brand.find(params[:id])
+    if @brand_cat.parent_id == 1
+      @abclist = Brand.new.abc_sort(@brand_cat)
+    end
+  end
+end
--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,3 +1,24 @@
 class Brand < ApplicationRecord
   has_ancestry
+  
+  def abc_sort(brand_cat)
+    children = brand_cat.children.sort{|a,b| a.name <=> b.name}
+    
+    topchar = children[0].name[0]
+    abclist = []
+    common_initial_brands = []
+    children.each do |c|
+      if c.name[0] == topchar
+        common_initial_brands << c
+      else
+        abclist << [topchar ,common_initial_brands]
+        common_initial_brands = []
+        # topchar と　その配列をセットし直す
+        topchar = c.name[0]
+        common_initial_brands << c
+      end
+    end
+    return abclist
+  end
+
 end
