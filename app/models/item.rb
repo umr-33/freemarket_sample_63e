83c56caf0fd543ef45dec4f744b484562feb0240
umr-33 <xx_yr@icloud.com>
2019-11-28 17:51:25 +0900

    comit

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
-  has_many :images
+  has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images
   has_many :images
   belongs_to :brand
