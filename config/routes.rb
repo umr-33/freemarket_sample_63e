d6382d9660778937a7130520c50312c449288d38
nana0707 <hydrangeaheartlessness@gmail.com>
2019-11-29 16:18:56 +0900

    商品削除２追加

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,9 @@ Rails.application.routes.draw do
       get "new4", to: "users#new4"
       get "new5", to: "users#new5"
     end
+    member do
+      get "listing"
+    end
   end
 
   resources :brands, only: [:index, :show]
