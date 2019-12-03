63eb65024c6e9793ac57534a87a90f63e5356cd0
hetiman0524 <heti0524.jdi@gmail.com>
2019-12-03 21:41:55 +0900

    軽微な変更

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -15,7 +15,7 @@ class CardsController < ApplicationController
       card: params['payjp-token'],
       metadata: {user_id: current_user.id}
       )
-      @card = CreditCard.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
+      @card = Cards.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
       if @card.save
         redirect_to action: "show"
       else
