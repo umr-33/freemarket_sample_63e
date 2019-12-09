82e8546f6d4ee86d156e51243acfb555d0447599
hetiman0524 <heti0524.jdi@gmail.com>
2019-12-09 15:08:17 +0900

    userログイン機能テスト

--- Gemfile
+++ Gemfile
@@ -46,6 +46,8 @@ group :development, :test do
   gem 'capistrano-bundler'
   gem 'capistrano-rails'
   gem 'capistrano3-unicorn'
+  gem 'rspec-rails'
+  gem 'rails-controller-testing'
 end
 
 group :development do
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+describe User do
+  describe '#create' do
+    it "is valid without a nickname" do
+     user = User.new(nickname: "", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
+     expect(user).to be_valid
+    end
+  end
+end
